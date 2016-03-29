0f38b92b2425dfc230554a8eff7e54c0c2596329
Elephant <temka.elephant@gmail.com>
2016-03-29 21:12:25 +0300

    dfjk

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,22 @@
+/**
+ * Created by elephant on 28.03.16.
+ */
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+public class GoogleTest {
+    @Test
+    public void userCanSearchKeywordWithGoogle(){
+       open("http://google.com");
+        $(By.name("q")).setValue("Selenide").pressEnter();
+
+        $$("#ires .g").shouldHave(CollectionCondition.size(10));
+        $("#ires .g").shouldHave(Condition.text("Selenide: лаконичные и стабильные UI тесты на Java"));
+
+    }
+
+}
